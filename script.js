c8163565ee9c05c8511d26989818d86de9cae0d6
Guilherme Betsa <ybtyza@gmail.com>
2024-03-09 15:58:35 -0300

    Removendo src pelo removeAttribute

--- script.js
+++ script.js
@@ -3,7 +3,5 @@ let div = document.getElementById('div')
 let img = div.querySelector('img')
 
 div.addEventListener('click', () => {
-        img.setAttribute('src', './mario.png')
-        img.style.width = '50vw'
-        img.style.height = '100vh'
+        img.removeAttribute('src', './mario.png')
 })
