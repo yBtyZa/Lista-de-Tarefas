6cdcb2cb823bdf043bdf81140135851433e4357d
Guilherme Betsa <ybtyza@gmail.com>
2024-03-10 17:28:32 -0300

    Lista de tarefas

--- script.js
+++ script.js
@@ -1,7 +1,64 @@
-
-let div = document.getElementById('div')
-let img = div.querySelector('img')
-
-div.addEventListener('click', () => {
-        img.removeAttribute('src', './mario.png')
+document.getElementById("form").addEventListener("click", function (event) {
+    event.preventDefault()
 })
+
+let arrayTarefas = []
+const tableTarefa = document.getElementById('tableTarefa')
+
+function adicionarTarefa() {
+    const tarefaValue = document.getElementById('tarefaValue').value
+    const selectValue = document.getElementById('selectValue').value
+
+    if (tarefaValue === '' || selectValue === 'selecione') {
+        alert('Valores inválidos')
+    }
+    else {
+        arrayTarefas.push({ tarefa: tarefaValue, dificuldade: selectValue })
+
+        exibirTarefa()
+    }
+}
+
+function exibirTarefa() {
+    tableTarefa.innerHTML = ''
+
+    arrayTarefas.forEach((element, index) => {
+        const novaLinha = document.createElement('tr')
+        novaLinha.setAttribute(`id`, `id-${index}`)
+
+        const novoElementoNome = document.createElement('th')
+        novoElementoNome.textContent = element.tarefa
+        novaLinha.appendChild(novoElementoNome)
+
+        const novoElementoDificuldade = document.createElement('th')
+        novoElementoDificuldade.textContent = element.dificuldade
+        novaLinha.appendChild(novoElementoDificuldade)
+
+        const btnExcluir = document.createElement('button')
+        btnExcluir.textContent = 'Excluir'
+        btnExcluir.setAttribute(`id`, `excluir-${index}`)
+        btnExcluir.setAttribute(`onclick`, `excluir(${index})`)
+        novaLinha.appendChild(btnExcluir)
+
+        const btnConcluir = document.createElement('button')
+        btnConcluir.textContent = 'Concluído'
+        btnConcluir.setAttribute(`id`, `concluir${index}`)
+        btnConcluir.setAttribute(`onclick`, `concluir(${index})`)
+        novaLinha.appendChild(btnConcluir)
+
+        tableTarefa.appendChild(novaLinha)
+
+    })
+
+}
+
+function excluir(index) {
+    arrayTarefas.splice(index, 1)
+    exibirTarefa()
+}
+
+function concluir(index) {
+    const concluirStyle = document.getElementById(`id-${index}`)
+    concluirStyle.style.textDecoration = 'line-through'
+    concluirStyle.style.opacity = '0.5'
+}
